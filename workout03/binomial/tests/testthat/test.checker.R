e302ae2f220eee066cf5b492c5aaaaf6a1dac960
Yeqing Chen <yech0095@berkeley.edu>
2019-05-01 17:39:54 -0700

    workout03: binomial package

--- workout03/binomial/tests/testthat/test.checker.R
+++ workout03/binomial/tests/testthat/test.checker.R
@@ -4,24 +4,24 @@ context("tests for checker functions")
 
 # Test check_prob
 test_that("the check_prob works with normal input",{
-  expect_equal(check_prob(0.5),TRUE)
-  expect_length(check_prob(0.2),1)
-  expect_type(check_prob(0.3), "logical")
-  
+  expect_equal(check_prob(prob = 0.5),TRUE)
+  expect_length(check_prob(prob = 0.5),1)
+  expect_type(check_prob(prob = 0.5), "logical")
+
 } )
 
 # Test check_trials
 test_that("the check_trials works with negative integer",{
-  expect_euqal(check_trials(-5),TRUE)
-  expect_length(check_trials(-5),1)
-  expect_type(check_trials(-5),"logical")
-  
+  expect_euqal(check_trials(trials = -5),TRUE)
+  expect_length(check_trials(trials = -5),1)
+  expect_type(check_trials(trials = -5),"logical")
+
 })
 
 # Test check_success
 test_that("the check_success works with normal input",{
-  expect_true(check_success(5,10))
-  expect_length(check_success(5,10),1)
-  expect_type(check_trials(15),"logical")
-  
+  expect_length(check_success(success = 5,trials = 10),1)
+  expect_type(check_success(success = 5,trials = 10),"logical")
+  expect_true(check_success(success = 5,trials = 10))
+
 } )
--- workout03/binomial/tests/testthat/test_summary_measures.R
+++ workout03/binomial/tests/testthat/test_summary_measures.R
@@ -5,35 +5,35 @@ context("Test for summary measures")
 
 # aux_mean function
 test_that("aux_mean works with normal inputs",{
-  expect_equal(aux_mean(10,0.3),3)
-  expect_length(aux_mean(10,0.3),1)
-  expect_type(aux_mean(10,0.3), "double")
+  expect_equal(aux_mean(trials = 10,prob = 0.3),3)
+  expect_length(aux_mean(trials = 10,prob = 0.3),1)
+  expect_type(aux_mean(trials = 10,prob = 0.3), "double")
 })
 
 # aux_variance function
 test_that("aux_variance works with normal inputs",{
-  expect_equal(aux_variance(10,0.3),2.1)
-  expect_length(aux_variance(10,0.3),1)
-  expect_type(aux_variance(10,0.3),"double")
+  expect_equal(aux_variance(trials = 10,prob = 0.3),2.1)
+  expect_length(aux_variance(trials = 10,prob = 0.3),1)
+  expect_type(aux_variance(trials = 10,prob = 0.3),"double")
 })
 
 # aux_mode function
 test_that("aux_mode works with normal inputs",{
-  expect_equal(aux_mode(10,0.3), 3)
-  expect_length(aux_mode(10,0.3),1)
-  expect_type(aux_mode(10,0.3),"integer")
+  expect_equal(aux_mode(trials = 10,prob = 0.3), 3)
+  expect_length(aux_mode(trials = 10,prob = 0.3),1)
+  expect_type(aux_mode(trials = 10,prob = 0.3),"integer")
 })
 
 # aux_skewness function
 test_that("aux_skewness works with normal inputs",{
-  expect_equal(aux_skewness(10,0.3),(1-2*0.3)/sqrt(10*0.3*(1-0.3)))
-  expect_length(aux_skewness(10,0.3),1)
-  expect_type(aux_skewness(10,0.3),"double")
+  expect_equal(aux_skewness(trials = 10,prob = 0.3),(1-2*0.3)/sqrt(10*0.3*(1-0.3)))
+  expect_length(aux_skewness(trials = 10,prob = 0.3),1)
+  expect_type(aux_skewness(trials = 10,prob = 0.3),"double")
 })
 
 # aux_kurtosis function
 test_that("aux_kurtosis works with normal inputs",{
-  expect_equal(aux_kurtosis(10,0.3),(1-6*0.3*(1-0.3))/(10*0.3*(1-0.3)))
-  expect_length(aux_kurtosis(10,0.3),1)
-  expect_type(aux_kurtosis(10,0.3),"double")
+  expect_equal(aux_kurtosis(trials = 10,prob = 0.3),(1-6*0.3*(1-0.3))/(10*0.3*(1-0.3)))
+  expect_length(aux_kurtosis(trials = 10,prob = 0.3),1)
+  expect_type(aux_kurtosis(trials = 10,prob = 0.3),"double")
 })
